7afcc51f8b1dc3b8caa7f7b9612cf631ef07ea17
신수지 <155224169+3suji3@users.noreply.github.com>
2026-02-02 00:30:44 +0900

    Title: 점수 비교, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260202/점수 비교/score-comparison.py
+++ 260202/점수 비교/score-comparison.py
@@ -1,5 +1,5 @@
-A1, B1 = map(int, input().split())
-A2, B2 = map(int, input().split())
+A1, A2 = map(int, input().split())
+B1, B2 = map(int, input().split())
 
 if A1 > B1 and A2 > B2:
     print(1)
