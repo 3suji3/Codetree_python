21788dfd47f8decefa34642f165ce1c2f2268d68
신수지 <155224169+3suji3@users.noreply.github.com>
2026-02-04 22:48:23 +0900

    Title: A 부터 B 까지, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260204/A 부터 B 까지/a-to-b.py
+++ 260204/A 부터 B 까지/a-to-b.py
@@ -3,11 +3,13 @@ A, B = map(int, input().split())
 print(A, end=" ")
 
 while True:
-    if A >= B:
-        break
     if A % 2 == 0:
+        if A + 3 >= B:
+            break
         A += 3
         print(A, end=" ")
     else:
+        if A * 2 >= B:
+            break
         A *= 2
         print(A, end=" ")
