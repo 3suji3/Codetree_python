fc42feee943154522ec55a08f154a04f1b3513f0
신수지 <155224169+3suji3@users.noreply.github.com>
2026-02-05 23:57:40 +0900

    Title: 좌우로 반복하며 출력, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260205/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,16 @@
+n = 4   
+
+for i in range(n):
+    row = []
+    
+    for j in range(1, n+1):
+        row.append(j)
+
+    if i % 2 != 0:
+        for j in range(n - 1, -1, -1):
+            print(row[j], end="")
+    else:
+        for j in range(n):
+            print(row[j], end="")
+    
+    print()
