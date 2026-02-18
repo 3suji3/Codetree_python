433abf7c374c1cf9f15e03709626dfa865d3955f
신수지 <155224169+3suji3@users.noreply.github.com>
2026-02-18 18:14:08 +0900

    Title: 두 사람, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260218/두 사람/two-person.py
+++ 260218/두 사람/two-person.py
@@ -1,10 +1,8 @@
-a = list(input().split())
-b = list(input().split())
+a = input().split()
+b = input().split()
 
-if int(a[0]) > 19 or int(b[0]) > 19:
-    if int(a[0]) > 19 and a[1] == 'M':
-        print(1)
-    elif int(b[0]) > 19 and b[1] == 'M':
-        print(1)
+if (int(a[0]) >= 19 and a[1] == 'M') or \
+   (int(b[0]) >= 19 and b[1] == 'M'):
+    print(1)
 else:
     print(0)
