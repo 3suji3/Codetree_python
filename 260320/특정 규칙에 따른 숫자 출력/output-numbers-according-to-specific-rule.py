3b4244bc22baf593cb92992ac56cd59a68d5c308
신수지 <155224169+3suji3@users.noreply.github.com>
2026-03-20 21:43:59 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260320/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,12 @@
+N = int(input())
+cnt = 1
+
+for i in range(N):
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(N-i, 0, -1):
+        print(cnt, end=" ")
+        cnt+=1
+        if cnt > 9:
+            cnt = cnt % 10 + 1
+    print()
