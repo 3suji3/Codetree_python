45ccbef115a25032eda2112fc51a13303f4bfc51
신수지 <155224169+3suji3@users.noreply.github.com>
2026-03-25 20:53:10 +0900

    Title: 특정 수 도달하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260325/특정 수 도달하기/reaching-specific-number.py
+++ 260325/특정 수 도달하기/reaching-specific-number.py
@@ -2,13 +2,13 @@ nums = []
 while len(nums) < 10:
     nums += list(map(int, input().split()))
 
-total = 0
-count = 0
+s = 0
+cnt = 0
 
-for n in nums:
-    if n >= 250:
+for x in nums:
+    if x >= 250:
         break
-    total += n
-    count += 1
+    s += x
+    cnt += 1
 
-print(total, total / count)
+print(s, round(s / cnt, 1))
