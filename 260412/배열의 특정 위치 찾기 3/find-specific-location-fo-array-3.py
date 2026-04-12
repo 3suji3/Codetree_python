fcef61be1eadeb65966503fd9ba87a74d6738fc7
신수지 <155224169+3suji3@users.noreply.github.com>
2026-04-12 17:24:35 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 260412/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,8 +1,8 @@
 n_list = list(map(int, input().split()))
 result = 0
-for i in n_list:
-    if i == 0:
+for i in range(len(n_list)):
+    if n_list[i] == 0:
+        result = n_list[i-3] + n_list[i-2] + n_list[i-1]
         break
-    result += i
 
 print(result)
